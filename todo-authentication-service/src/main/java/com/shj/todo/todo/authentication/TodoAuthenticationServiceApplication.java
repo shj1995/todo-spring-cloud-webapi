8d7d15740ea46178c11b9828e69cbff8cc360385
shihaojie <517925985@qq.com>
2019-10-22 20:35:49 +0800

    fix

---
+++ todo-authentication-service/src/main/java/com/shj/todo/todo/authentication/TodoAuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shj.todo.todo.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAuthenticationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoAuthenticationServiceApplication.class, args);
+    }
+
+}
